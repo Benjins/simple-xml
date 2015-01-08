a2f7f9d430b21e18329e1d1f0213fd7df749f216
Benji Smith <smith.benj@husky.neu.edu>
2015-01-08 13:57:43 -0500

    Updated license.

--- simple-xml.h
+++ simple-xml.h
@@ -1,13 +1,6 @@
 /*
 Author: Benji Smith
-Licensed under Creative Commons Attribution 4.0 International (CC BY 4.0)
-creativecommons.org/licenses/by/4.0/
-
-You are free to:
-Share — copy and redistribute the material in any medium or format
-Adapt — remix, transform, and build upon the material for any purpose, even commercially.
-
-The licensor cannot revoke these freedoms as long as you follow the license terms.
+Licensed under MIT License. See License.txt for details
 */
 
 #include <vector>
